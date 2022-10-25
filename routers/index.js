f3f44fa073925dd74f88b11193e54c6d5826babd
Javoxir1501 <javoxirqamariddinov006@gmail.com>
2022-10-25 05:04:19 +0500

    indexjsda nastroyka qilindi

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
+const movieDb = require("../model/work")
 
 router.get("/api/movies", (req, res)=>{
-
+    movieDb.find({}, (error, data)=>{
+        req.send(data)
+    })
 })
 
 module.exports = router
